bf31f53595b8ba3ac08a86029d1aafa65bcd6fe2
ZexTechX <sigmajawadx@gmail.com>
2025-06-24 07:49:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "IK~8WEWDaxS#m2DHBDXydPDlV4oko72g9uo-KVEbsJoavE2YOH1ZOXk",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "IK~UCkGQLpa#JamXqBQvIuSGwj8M8G32RJDSpOXNqCGZ58bJCJ4obXg",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "KHAN-MD",  // Bot's display name
